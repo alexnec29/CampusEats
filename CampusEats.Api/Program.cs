dc36be55ca065c3571bac88288aa8a43753476f8
Alexandru Nechifor <alexandrunec29@gmail.com>
2025-11-27 09:24:12 +0200

    Update Program.cs

--- CampusEats.Api/Program.cs
+++ CampusEats.Api/Program.cs
@@ -2,7 +2,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using CampusEats.Api.Behaviors;
 using CampusEats.Api.Endpoints;
-using CampusEats.Api.Features.Allergen;
+using CampusEats.Api.Features.KitchenTask;
 using CampusEats.Api.Infrastructure;
 using CampusEats.Api.Infrastructure.Repositories;
 using CampusEats.API.Infrastructure.Repositories;
@@ -116,6 +116,7 @@ app.MapTestEndpoints();
 app.MapUserEndpoints();
 app.MapOrderEndpoints();
 app.MapAllergenEndpoints();
+app.MapKitchenEndpoints();
 
 using (var scope = app.Services.CreateScope())
 {
