314a7ad9e7f9670b92d556f3bcae08d7f48b9df9
Alexandru Nechifor <alexandrunec29@gmail.com>
2025-11-27 08:58:26 +0200

    Update user endpoint routes

--- CampusEats.Api/Endpoints/UserEndpoints.cs
+++ CampusEats.Api/Endpoints/UserEndpoints.cs
@@ -7,27 +7,27 @@ public static class UserEndpoints
 {
     public static void MapUserEndpoints(this WebApplication app)
     {
-        var users = app.MapGroup("/user")
+        var users = app.MapGroup("api/user")
             .WithTags("Users")
             .WithOpenApi();
 
-        users.MapPost("/api/user/register", async (CreateUserRequest request, IMediator mediator) =>
+        users.MapPost("/register", async (CreateUserRequest request, IMediator mediator) =>
             await mediator.Send(request)).AllowAnonymous();
 
-        users.MapPost("/api/user/login",
+        users.MapPost("/login",
             async (HttpContext httpContext, LoginUserRequest request, IMediator mediator) =>
             {
                 if (httpContext.User.Identity?.IsAuthenticated == true) return Results.Ok("User already logged in");
                 return await mediator.Send(request);
             }).AllowAnonymous();
 
-        users.MapPost("/api/user/logout", async (LogoutUserRequest request, IMediator mediator) =>
+        users.MapPost("/logout", async (LogoutUserRequest request, IMediator mediator) =>
             await mediator.Send(request)).RequireAuthorization("AllRoles");
 
-        users.MapPut("/api/user/update-buyer-profile", async (UpdateBuyerProfileRequest request, IMediator mediator) =>
+        users.MapPut("/update-buyer-profile", async (UpdateBuyerProfileRequest request, IMediator mediator) =>
             await mediator.Send(request)).RequireAuthorization("Buyer");
 
-        users.MapPut("/api/user/update-kitchen-profile",
+        users.MapPut("/update-kitchen-profile",
             async (UpdateKitchenProfileRequest request, IMediator mediator) =>
                 await mediator.Send(request)).RequireAuthorization("Kitchen");
     }
