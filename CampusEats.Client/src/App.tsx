fd1c554ee8212c7a89a918f85a7473fe2777102b
Stoian Cristian <stoiancristian2002@gmail.com>
2025-12-02 21:51:53 +0200

    added typescript/tailwindcss setup

---
+++ CampusEats.Client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <h2>Bine ai venit la CampusEats!</h2>
+      <p>Aici va fi conținutul principal al paginii tale.</p>
+    </Layout>
+  );
+}
+
+export default App;
