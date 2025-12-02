14dd14139ab21ae7550bde2a5557492307ab3b1d
Stoian Cristian <stoiancristian2002@gmail.com>
2025-12-02 22:38:36 +0200

    React config ready

--- CampusEats.Client/wwwroot/js/cookie.js
+++
@@ -1,6 +0,0 @@
-window.getCookie = (name) => {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
-    return null;
-};
